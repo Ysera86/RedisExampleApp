dc6561c24f617a2e124702aef38da6c87fb3f109
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-09 09:26:59 +0300

    Add : DbContext to use EfCore and EfCoreInMempryDatabase Add : Entity model Product

---
+++ RedisExampleApp.API/Models/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RedisExampleApp.API.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product() { Id = 1, Name = "Kalem", Price = 10 },
+                new Product() { Id = 2, Name = "Kitap", Price = 20 },
+                new Product() { Id = 3, Name = "Defter", Price = 30 }
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
