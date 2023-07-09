95c9913313ae4238ac84d04ae861538507753bcc
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-09 11:14:18 +0300

    Implement Decorator Design Pattern Modify ProductRepositoryWithCache into ProductRepositoryWithCacheDecorator and implement methods

--- RedisExampleApp.API/Repository/ProductRepositoryWithCache.cs
+++ RedisExampleApp.API/Repository/ProductRepositoryWithCache.cs
@@ -1,34 +1,81 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 using RedisExampleApp.API.Models;
 using RedisExampleApp.Cache;
+using StackExchange.Redis;
+using System.Text.Json;
 
 namespace RedisExampleApp.API.Repository
 {
-    public class ProductRepositoryWithCache : IProductRepository
+    public class ProductRepositoryWithCacheDecorator : IProductRepository
     {
         private readonly IProductRepository _productRepository;
         private readonly RedisService _redisService; // adı redisRepo olabilirdi.
 
-        public ProductRepositoryWithCache(IProductRepository productRepository, RedisService redisService)
+        private readonly IDatabase _cacheRepository;
+
+        private const string productKey = "productCache"; // hash key
+
+        public ProductRepositoryWithCacheDecorator(IProductRepository productRepository, RedisService redisService)
         {
             _productRepository = productRepository;
             _redisService = redisService;
+
+            _cacheRepository = _redisService.GetDb(1);
         }
 
-        public Task<Product> CreateAsync(Product product)
+        public async Task<Product> CreateAsync(Product product)
         {
-            throw new NotImplementedException();
+            var newProduct = await _productRepository.CreateAsync(product);
+
+            if (await _cacheRepository.KeyExistsAsync(productKey))
+            {
+                await _cacheRepository.HashSetAsync(productKey, product.Id, JsonSerializer.Serialize(newProduct));
+            }
+
+            return newProduct;
         }
 
-        public Task<List<Product>> GetAllAsync()
+        public async Task<List<Product>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            if (!await _cacheRepository.KeyExistsAsync(productKey))
+            {
+                return await LoadToCacheFromDbAsync();
+            }
+
+            var products = new List<Product>();
+            foreach (var item in (await _cacheRepository.HashGetAllAsync(productKey)).ToList())
+            {
+                var product = JsonSerializer.Deserialize<Product>(item.Value);
+
+                products.Add(product);
+            }
+
+            return products;
         }
 
-        public Task<Product> GetByIdAsync(int id)
+        public async Task<Product> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            if (!await _cacheRepository.KeyExistsAsync(productKey))
+            {
+                var product = await _cacheRepository.HashGetAsync(productKey, id);
+                return product.HasValue ? JsonSerializer.Deserialize<Product>(product) : null;
+            }
+
+            var products = await LoadToCacheFromDbAsync();
+
+            return products.FirstOrDefault(x => x.Id == id);
+        }
+
+        // dbden al redise cache
+        private async Task<List<Product>> LoadToCacheFromDbAsync()
+        {
+            var products = await _productRepository.GetAllAsync();
+            products.ForEach(product =>
+            {
+                _cacheRepository.HashSetAsync(productKey, product.Id, JsonSerializer.Serialize(product));
+            });
+
+            return products;
         }
     }
 }
